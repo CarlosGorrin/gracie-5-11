ae23f0a158a07024803d93ef40d2033bec54b45b
speedtetris <speedtetris@gmail.com>
2015-11-08 22:59:56 +0000

    Modified Gemfile for heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module SimplecodecastsSaas
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 config.assets.initialize_on_precompile = false
+    #config.active_record.raise_in_transactional_callbacks = true
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -21,6 +22,6 @@ config.assets.initialize_on_precompile = false
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
