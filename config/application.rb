20ed9c7bae996903b138efa2fa8880a8cda2397a
speedtetris <speedtetris@gmail.com>
2015-11-11 02:48:14 +0000

    Reorganized css

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ config.assets.initialize_on_precompile = false
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
